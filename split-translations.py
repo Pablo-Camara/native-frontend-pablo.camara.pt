3ddca9e7889843c4ad1d0e08742a0a597f2770b9
Pablo Camara <pablo.camara.pt@gmail.com>
2020-11-06 18:02:51 +0000

    Welcome bar + handle translations

---
+++ split-translations.py
@@ -0,0 +1,13 @@
+import json
+
+with open('assets/json/translations/translations.json') as json_file:
+    data = json.load(json_file)
+    languageData = {}
+    for translationStringId in data:
+        for translationLanguageCode in data[translationStringId]:
+            languageData[translationLanguageCode] = {}
+            languageData[translationLanguageCode][translationStringId] = data[translationStringId][translationLanguageCode];
+    
+    for languageCode in languageData:
+        with open('public/assets/json/translations_' + languageCode + '.json', 'w') as outfile:
+                json.dump(languageData[languageCode], outfile)
