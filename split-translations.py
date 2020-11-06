13172c6fc69ef5c1aed210ee1490f72131123e42
Pablo Camara <pablo.camara.pt@gmail.com>
2020-11-06 21:07:19 +0000

    fix translationg splitting + add translation strings

--- split-translations.py
+++ split-translations.py
@@ -5,9 +5,12 @@ with open('assets/json/translations/translations.json') as json_file:
     languageData = {}
     for translationStringId in data:
         for translationLanguageCode in data[translationStringId]:
-            languageData[translationLanguageCode] = {}
+            if translationLanguageCode not in languageData:
+                languageData[translationLanguageCode] = {}
+
             languageData[translationLanguageCode][translationStringId] = data[translationStringId][translationLanguageCode];
     
     for languageCode in languageData:
         with open('public/assets/json/translations_' + languageCode + '.json', 'w') as outfile:
                 json.dump(languageData[languageCode], outfile)
+    
